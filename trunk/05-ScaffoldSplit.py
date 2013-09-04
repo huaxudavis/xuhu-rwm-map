dee093e9d220ee4f90e7a8acd18617a00f10ec75
huaxudavis <huaxudavis@0d85c069-4d14-3584-9d17-49e286a932cb>
2013-09-04 00:19:04 +0000

    add start and end positions and number of items per group

--- trunk/05-ScaffoldSplit.py
+++ trunk/05-ScaffoldSplit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 ##################################################################################
-# Author: Huaqin Xu (huaxu@ucdavis.edu)
-# Date: Aug.16 2013
+# Atuthor: Lutz Froenicke(lfroenicke@ucdavis.edu) and Huaqin Xu (huaxu@ucdavis.edu)
+# Date: Aug.16 2013, last update: Aug.29 2013
 # Description:
 #
 # This python script split the SNP chunk files by the turning points 'A'<->'B'  
@@ -22,7 +22,6 @@ from os.path import basename, splitext
 
 ######################################################
 
-
 # ----- get options and file names and open files -----
 if len(sys.argv) == 4:
     infile = sys.argv[1]
@@ -42,18 +41,23 @@ tsvout = open(outfile, 'wb')
 tsvinreader = csv.reader(tsvin, delimiter='\t')
 tsvoutwriter = csv.writer(tsvout, delimiter='\t')
 
-
-# ----- main: count the occurance of 'A','B','-' --------
+# ----- main: count the occurance of 'A','B','-','U'--------
 
 cntU = 0
 start = 1
 prerow =[]
+urow = []
 rowlist = []
+scafcnt = 0
+splitcnt = 0
 
 for row in tsvinreader:
     if len(row) == 0 or row[0] == '':
-        for r in rowlist: # print the last part of the previous scaffold
-            tsvoutwriter.writerow([curid] + r[1:])
+        if rowlist != []: 
+            for r in rowlist: # print the last part of the previous scaffold
+                tsvoutwriter.writerow([curid] + r[1:])
+            scafcnt = scafcnt + 1
+            splitcnt = splitcnt + 1
         rowlist=[]
         tsvoutwriter.writerow([])
         start = 1
@@ -63,31 +67,43 @@ for row in tsvinreader:
         
         if start == 1:   # initial values if it is the first line of scaffold
             curid = id
-            prerow = row[2:]
+            prerow = row[4:]
             start = 0
             sub = 1
-            rowlist.append(row) 
+            rowlist.append(row)
+
         else:
-            cntU = row[2:].count('U') # count the occurence of 'U'
-            mismatches = len([x for x, y in zip(prerow, row[2:]) if x != y]) # find mismatch items in previous and current rows   
-            prerow = row[2:]        
+            cntU = row[4:].count('U') # count the occurence of 'U'
+            mismatches = len([x for x, y in zip(prerow, row[4:]) if x != y]) # find mismatch items in previous and current rows         
             
+            if cntU >= cutoff:
+                urow = row
+                row = tsvinreader.next()
+                mismatches = len([x for x, y in zip(prerow, row[4:]) if x != y])
+            prerow = row[4:]
+     
             if mismatches > cutoff: # split the mismatch parts
-                if cntU < cutoff:   # if occurance of 'U' < cutoff
-                    curid = id + '_' + str(sub)
-                    for r in rowlist: # print part of scaffold 
-                        tsvoutwriter.writerow([curid] + r[1:])
-                    tsvoutwriter.writerow([])
-                    tsvoutwriter.writerow([])
-                    
-                    sub = sub + 1 # prepare for the next part of scaffold
-                    curid = id + '_' + str(sub)
-                    rowlist = []
-                    rowlist.append(row)        
-            else: 
+                curid = id + '_' + str(sub)
+                for r in rowlist: # print part of scaffold 
+                    tsvoutwriter.writerow([curid] + r[1:])
+                tsvoutwriter.writerow([])
+                tsvoutwriter.writerow([])
+                splitcnt = splitcnt + 1
+                
+                sub = sub + 1 # prepare for the next part of scaffold
+                curid = id + '_' + str(sub)
+                rowlist = []
+                rowlist.append(row)                    
+            else:
+                if cntU >= cutoff: 
+                    rowlist.append(urow)
                 rowlist.append(row)
+                    
                 
 for r in rowlist: # last part of last scaffold
     tsvoutwriter.writerow([curid] + r[1:])
+scafcnt = scafcnt + 1
+splitcnt = splitcnt + 1    
 
-
+print "Processed Scaffolds:" + str(scafcnt)
+print "New Scaffold segments:" + str(splitcnt)
