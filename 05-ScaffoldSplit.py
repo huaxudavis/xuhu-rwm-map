02c7c28fc8cf3d567c24cf73413aa4ecfc196455
huaxudavis <huaxudavis@0d85c069-4d14-3584-9d17-49e286a932cb>
2013-11-05 00:53:45 +0000

    Change how to calculate mismatch, and change the cutoff to ratio instead of absolute value. Fix the bug to deal with empty line.

--- 05-ScaffoldSplit.py
+++ 05-ScaffoldSplit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 ##################################################################################
 # Author: Lutz Froenicke(lfroenicke@ucdavis.edu) and Huaqin Xu (huaxu@ucdavis.edu)
-# Date: Aug.16 2013, last update: Oct.4 2013
+# Date: Aug.16 2013, last update: Nov.4 2013
 # Description:
 #
 # This python script split the SNP chunk files by the turning points 'A'<->'B'
@@ -10,38 +10,38 @@
 # input arguments:
 #   1.input file.
 #   2. number of samples
-#   3. cutoff for mismatches 
-#   3. cutoff for 'U' (for example: 25)
-#   4. cutoff for '-' (missing data)
+#   3. cutoff for mismatches (ratio of mismatches/matches between A and B genotype callls - please provide a value in the range > 0 and < 1 )
+#   4. cutoff for 'U' (for example: 25)
 #
 # Output: split file.
+
+# usage: python 05-ScaffoldSplit.py genotypetable.tsv 88 0.1 10
 #
 ######################################################################################
 
 import csv, os, sys
 from os.path import basename, splitext
 
-
 ######################################################
 
 # ----- get options and file names and open files -----
-if len(sys.argv) == 6:
+if len(sys.argv) == 5:
     infile = sys.argv[1]
     samples = int(sys.argv[2])
-    cutoff = int(sys.argv[3])
+    cutoff = float(sys.argv[3])
     cutoffU = int(sys.argv[4])
-    cutoffM = int(sys.argv[5])
+#    cutoffM = int(sys.argv[5])
 else:
     print len(sys.argv)
-    print "Usage: [1]infile, [2]# of samples, [3]cutoff of missmatches, [4]cutoff of 'U', [5]cutoff of '-'"
+    print "Usage: [1]infile, [2]# of samples, [3]cutoff of missmatches, [4]cutoff of 'U'" #, [5]cutoff of '-'"
     sys.exit(1)
 
 infbase = splitext(basename(infile))[0]
-outfile = 'split_c' + sys.argv[3] + '-' + sys.argv[4] + '-' + sys.argv[5] + infbase + '.tsv'
+outfile = 'split_AB_' + sys.argv[3] + '-' + sys.argv[4] + infbase + '.tsv'  #  + '-' + sys.argv[5]
 
 tsvin = open(infile,'rb')
 tsvout = open(outfile, 'wb')
-log = open("log_split_c" + sys.argv[3] + '-' + sys.argv[4] + '-' + sys.argv[5] + infbase + '.txt','w')
+log = open("log_split_AB_" + sys.argv[3] + '-' + sys.argv[4] + infbase + '.txt','w') # + '-' + sys.argv[5]
 
 tsvinreader = csv.reader(tsvin, delimiter='\t')
 tsvoutwriter = csv.writer(tsvout, delimiter='\t')
@@ -60,7 +60,10 @@ for row in tsvinreader:
     if len(row) == 0 or row[0] == '':
         if rowlist != []:
             for r in rowlist: # print the last part of the previous scaffold
-                tsvoutwriter.writerow([curid] + r[1:])
+                if(len(row)!= ''):
+                    tsvoutwriter.writerow([curid] + r[1:])
+                else:
+                    tsvoutwriter.writerow(r)
             scafcnt = scafcnt + 1
             splitcnt = splitcnt + 1
         rowlist=[]
@@ -68,45 +71,50 @@ for row in tsvinreader:
         start = 1
     else:
         id = row[0]
+        matches = 0
         mismatches = 0
 
         if start == 1:   # initial values if it is the first line of scaffold
             curid = id
-            prerow = row[4:]
+            prerow = row
+
             start = 0
             sub = 1
             rowlist.append(row)
 
         else:
             cntU = row[4:].count('U') # count the occurence of 'U'
-            cntM = row[4:].count('-') # count the occurence of '-'
-            mismatches = len([x for x, y in zip(prerow, row[4:]) if x != y]) # find mismatch items in previous and current rows
+        #    cntM = row[4:].count('-') # count the occurence of '-'
+            matches = len([x for x, y in zip(prerow[4:], row[4:]) if ( x == 'A' and y == 'A' ) or ( x == 'B' and y == 'B' ) or ( x == 'U' and y == 'U' )]) # find match items in previous and current rows
+            mismatches = len([x for x, y in zip(prerow[4:], row[4:]) if ( x == 'A' and y == 'B' ) or ( x == 'B' and y == 'A' )]) # find mismatch items in previous and current rows
 
             if cntU >= cutoffU:
                 urow = row
                 row = tsvinreader.next()
-                mismatches = len([x for x, y in zip(prerow, row[4:]) if x != y])
-            prerow = row[4:]
+                if len(row) != 0 and row[0] != '':
+                    matches = len([x for x, y in zip(prerow[4:], row[4:]) if ( x == 'A' and y == 'A' ) or ( x == 'B' and y == 'B' ) or ( x == 'U' and y == 'U' )])
+                    mismatches = len([x for x, y in zip(prerow[4:], row[4:]) if ( x == 'A' and y == 'B' ) or ( x == 'B' and y == 'A' )])
 
-            if mismatches > cutoff and cntM < cutoffM: # split the mismatch parts
-                curid = id + '_' + str(sub)
-                log.write(str([curid]) + str(row[2]) + '\n') # write the split position to log file
+            if  float(mismatches)/(matches+mismatches) > cutoff and len(row) != 0 and row[0] != '': # split the mismatch parts
+                curid = id + '__' + str(sub)
+                log.write(id + "\t" + str(prerow[2]) + '\n') # write the split position to log file
                 for r in rowlist: # print part of scaffold
                     tsvoutwriter.writerow([curid] + r[1:])
 
-                tsvoutwriter.writerow([])
-                tsvoutwriter.writerow([])
+                breakline = ['']+ [''] +['I']*(samples+2)
+                tsvoutwriter.writerow(breakline)
+                tsvoutwriter.writerow(breakline)
                 splitcnt = splitcnt + 1
 
                 sub = sub + 1 # prepare for the next part of scaffold
-                curid = id + '_' + str(sub)
+                curid = id + '__' + str(sub)
                 rowlist = []
                 rowlist.append(row)
             else:
                 if cntU >= cutoffU:
                     rowlist.append(urow)
                 rowlist.append(row)
-
+            prerow = row
 
 for r in rowlist: # last part of last scaffold
     tsvoutwriter.writerow([curid] + r[1:])
@@ -119,6 +127,6 @@ print "New Scaffold segments:" + str(splitcnt)
 log.write("\n\n")
 log.write("Filename:" + sys.argv[1] + '\n')
 log.write("Number Of Samples:" + sys.argv[2] + '\n')
-log.write("Cutoff Threshold:" + sys.argv[3] + '-' + sys.argv[4] + '-' + sys.argv[5]+'\n')
+log.write("Cutoff Threshold:" + sys.argv[3] + '-' + sys.argv[4] +'\n')  # + '-' + sys.argv[5]
 log.write("Processed Scaffolds:" + str(scafcnt) + '\n')
 log.write("New Scaffold segments:" + str(splitcnt)+ '\n')
